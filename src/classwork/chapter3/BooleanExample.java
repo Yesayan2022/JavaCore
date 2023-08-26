bc30d4aced3dd60191532ce116e1e295ae20ac3f
Narine Yesayan <yesayan2022@gmail.com>
2023-08-27 00:11:03 +0400

    Classes

---
+++ src/classwork/chapter3/BooleanExample.java
@@ -0,0 +1,11 @@
+package classwork.chapter3;
+
+public class BooleanExample {
+    public static void main(String[] args) {
+       int a = 10;
+       int b = 50;
+       boolean t= a > b;
+        System.out.println(t);
+
+    }
+}
