f91ed6053369e422a0cec2f43d1b519d667db9c9
Narine Yesayan <yesayan2022@gmail.com>
2023-08-30 17:24:56 +0400

    Classes

---
+++ src/homework1/FigurePainter.java
@@ -0,0 +1,75 @@
+package homework1;
+
+public class FigurePainter {
+    public static void main(String[] args) {
+        for (int i = 0; i < 6; i++) {
+
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int i = 5; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+// pordz
+        for (int i = 0; i < 6; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print("  ");
+
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+
+            }
+            for (int j = 5; j > i; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        for (int i = 0; i < 5; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print(" ");
+
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+        }
+
+
+        for (int i = 0; i < 5; i++) {
+
+
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+
+            }
+            for (int j = 5; j > i; j--) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+        }
+
+    }
+}
+
