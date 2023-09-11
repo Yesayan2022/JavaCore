3219f037390523a682816f2101b6d37aa7f48d90
Narine Yesayan <yesayan2022@gmail.com>
2023-09-11 21:06:08 +0400

    Classes

---
+++ src/classwork/chapter4/Ternary.java
@@ -0,0 +1,15 @@
+package classwork.chapter4;
+
+public class Ternary {
+    public static void main(String[] args) {
+        int i,k;
+        i=10;
+        k= i < 0 ? -i:i;
+        System.out.print(" Absolut value is ");
+        System.out.println(i+" equal "+k);
+        i=-10;
+        k=i<0 ? -i: i;
+        System.out.print(" Absolut value is ");
+        System.out.println(i+" equal "+k);
+    }
+}
---
+++ src/classwork/chapter6/BoxTest.java
@@ -0,0 +1,12 @@
+package classwork.chapter6;
+
+public class BoxTest {
+    public static void main(String[] args) {
+        Box myBox = new Box();
+        myBox.depth = 8.8;
+        System.out.println(myBox.depth);
+        System.out.println(myBox.height);
+        System.out.println(myBox.width);
+    }
+
+}
